dd2a41b691a4f20198ab8659a1f9352d346d38db
monish <monish.ghutke@gmail.com>
2016-12-05 01:29:59 +0530

    Custom endpoints for grids.

--- common/models/bank.js
+++ common/models/bank.js
@@ -2,4 +2,61 @@
 
 module.exports = function(Bank) {
 
+	/*endpoint grid*/
+
+	Bank.getBanks = function(city, callback) {
+		
+		var response = [];
+		var options = { include: ['tcexes', 'tades'],
+				fields: { ifscCode: true, branchCode: true,
+					  bankName: true, branchName: true,
+					  city: true
+					}
+			      };
+		if(city !== undefined) {
+			options.where = {'city': city };
+		}
+
+		Bank.find(options, function(err, transactions){
+			transactions.forEach(function(transaction){
+				var t = transaction.toJSON();
+				
+				/*summation logic*/
+				t.sumCex = Bank.sumTotalCex(t.tcexes);
+				t.sumAde = Bank.sumTotalAde(t.tades);
+				t.total = t.sumCex + t.sumAde;
+				delete t.tcexes;
+				delete t.tades;
+				
+				response.push(t);
+			});
+			callback(null, response);
+		});
+	}
+
+	Bank.sumTotalCex = function(transactions){
+		
+		var sum = 0;
+		transactions.forEach(function(transaction){
+			sum += parseInt(transaction.totalCex);
+		});
+		return sum;
+	};
+
+	
+	Bank.sumTotalAde = function(transactions){
+		
+		var sum = 0;
+		transactions.forEach(function(transaction){
+			sum += parseInt(transaction.totalAde);
+		});
+		return sum;
+	};
+
+	Bank.remoteMethod('getBanks', {
+		accepts: {arg: 'city', type: 'string'},	
+		http: {path: '/grid', verb: 'get'},
+		returns: {type: 'array', root: true}
+	});
+
 };
--- common/models/person.js
+++ common/models/person.js
@@ -1,5 +1,62 @@
 'use strict';
 
 module.exports = function(Person) {
+	
+	/*endpoint grid*/
 
+	Person.getIndividuals = function(city, callback) {
+		
+		var response = [];
+		var options = { include: ['tcexes', 'tades'],
+				fields: { panId: true, aadhaarId: true,
+					  name: true, avgItrIncome: true,
+					  city: true, id: true
+					}
+			      };
+
+		if(city !== undefined) {
+			options.where = {'city': city };
+		}
+
+		Person.find(options, function(err, transactions){
+			transactions.forEach(function(transaction){
+				var t = transaction.toJSON();
+				
+				/*summation logic*/
+				t.sumCex = Person.sumTotalCex(t.tcexes);
+				t.sumAde = Person.sumTotalAde(t.tades);
+				t.total = t.sumCex + t.sumAde;
+				delete t.tcexes;
+				delete t.tades;
+				
+				response.push(t);
+			});
+			callback(null, response);
+		});
+	}
+
+	Person.sumTotalCex = function(transactions){
+		
+		var sum = 0;
+		transactions.forEach(function(transaction){
+			sum += parseInt(transaction.totalCex);
+		});
+		return sum;
+	};
+
+	
+	Person.sumTotalAde = function(transactions){
+		
+		var sum = 0;
+		transactions.forEach(function(transaction){
+			sum += parseInt(transaction.totalAde);
+		});
+		return sum;
+	};
+
+	Person.remoteMethod('getIndividuals', {
+		accepts: {arg: 'city', type: 'string'},	
+		http: {path: '/grid', verb: 'get'},
+		returns: {type: 'array', root: true}
+	});
 };
